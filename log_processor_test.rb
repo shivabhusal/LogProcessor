f91a9c24f14f51fd1c73b1d4b105fca5636f44e9
Shiva / John <hotline.shiva@gmail.com>
2017-05-16 10:23:27 +0545

    Log Processor: Init commit: Almost ready with Test coverage

---
+++ log_processor_test.rb
@@ -0,0 +1,32 @@
+require 'rspec'
+require './processor.rb'
+
+RSpec.describe LogProcessor do
+  it 'should validate if the file is a valid log file' do
+    `echo "this is an invalid log format" >> invalid.log`
+    lp = LogProcessor.new('invalid.log')
+    lp.process
+    expect(lp.valid?).to eq(false)
+    `rm invalid.log`
+  end
+
+  it 'should validate as true if file is a valid log' do
+    `echo "2016-12-14 this is a log line 1 [warning]\n2016-12-15 this is a log line 2 [warning]" >> valid.log`
+    lp = LogProcessor.new('valid.log')
+    lp.process
+    expect(lp.valid?).to eq(true)
+    `rm valid.log`
+  end
+
+
+  it 'should parse the content of each line and store in a data structure' do
+    `echo "2016-12-14 this is a log line 1 [warning]\n2016-12-14  line 2 [error]\n2016-12-14 thig line 3 [error]\n2016-12-15 og line 3 [error]" >> valid.log`
+    lp = LogProcessor.new('valid.log')
+    lp.process
+    expect(lp.data).to eq({'2016-12-14' => {'warning' => 1, 'error' => 2}, '2016-12-15' => {'warning' => 0, 'error' => 1}})
+    `rm valid.log`
+  end
+
+  it 'should calculate the report properly'
+  it 'should render the expected output in STDOUT stream'
+end
---
+++ processor.rb
@@ -0,0 +1,43 @@
+#! /usr/bin/ruby
+# to run your code from CLI you need to use proper `shebang` like the line above
+
+class LogProcessor
+  attr_reader :log_file, :data
+  DATE_REGEX = /\d{4}\-\d{1,2}\-\d{1,2}/
+  LOG_LEVEL_REGEX =  /(?<=\[)\w+(?=\])/
+
+  def initialize(file)
+    @log_file = file
+    @data = {} # format: {date: {warning: 0, error: 1}}
+    @validity = true
+  end
+  
+  def valid?
+    @validity  # making sure it does not return `nil`
+  end
+
+  def process
+    parse
+  end
+
+  private
+
+  
+  def parse
+    File.open(log_file, 'r').each_line do |line|
+      parse_line(line)  
+    end
+  end
+
+  def parse_line(line)
+    date = line[DATE_REGEX]
+    log_level = line[LOG_LEVEL_REGEX]
+
+    data[date] = {'warning' => 0, 'error' => 0} if data[date].nil?
+    data[date][log_level] = 0 if data[date][log_level].nil?
+
+    data[date][log_level] = data[date][log_level].nil? ? 1 :   data[date][log_level] + 1
+
+    @validity = false if date.nil? || log_level.nil?
+  end
+end
