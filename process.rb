1cc6caf49ea5d58ff13ccf25e40f7f2460c3e6d2
Shiva / John <hotline.shiva@gmail.com>
2017-05-16 11:03:06 +0545

    Can be executed from commandline

---
+++ process.rb
@@ -0,0 +1,11 @@
+require './processor'
+
+file_names_via_arg = ARGV;
+file_names_via_arg.each do |filename|
+ puts "In file: '#{filename}'"
+ puts '*'*80
+ lp =  LogProcessor.new(filename)
+ lp.process
+ puts '-'*80
+end
+
